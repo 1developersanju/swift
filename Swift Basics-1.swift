a61916209d0ee5dc57db94b825bcce089ad73340
1developersanju <30802329+1developersanju@users.noreply.github.com>
2021-01-26 12:57:19 +0530

    Add files via upload

---
+++ Swift Basics-1.swift
@@ -0,0 +1,37 @@
+// void which means no return value.
+
+import UIKit
+
+//UIKit is a framework, which defines the core components of ios applicationsprovided by apple for developing ios apps from labels and buttons to table views and navigation controllers.
+
+var str = "Hello, Everyone"
+str = "My first PlayGround"
+print (str)
+
+//Declaring a var or const,and if we asign a peace of data to it....the variable takes the data type of the perticular data to store in it,For eg var a = 32,this 'a' takes the data type of the 32 'string',so it does not store any other data types such as bool,double..
+
+var a = 32
+a = -42
+
+//a = 890.88
+//for decimal values,we can use - float or decimal.
+
+var b = 32.00
+b = -42.00
+b = 890.88
+
+var c = 34.534
+print(str,"the values are :",a,",",b,"and",c)
+
+//var d: String = 32
+var d: String = "this format only stores string"
+
+//var e: Bool = 42
+var e: Bool = true // this format only stores boolean values
+
+print(d,",",e)
+
+// math multiplication
+var f = 3
+var g = 4
+print(f*g)
